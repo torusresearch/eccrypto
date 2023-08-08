a4f4995d5402f339508618b3c2ed3fa3c1fbb7bf
Archit <arch1995@gmail.com>
2023-08-08 10:38:13 +0530

    Fix eslint errors in test file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: ["@toruslabs/eslint-config-typescript"],
+  extends: ["@toruslabs/eslint-config-node"],
   parser: "@typescript-eslint/parser",
   ignorePatterns: ["*.config.js", ".eslintrc.js"],
   parserOptions: {
